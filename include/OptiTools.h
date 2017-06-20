6db5ecc0822c37432696a652166954cbd262e466
jk <jk@airsensing.com>
2017-06-20 18:13:56 -0400

    still trying to clear out this detached head mess

--- include/OptiTools.h
+++ include/OptiTools.h
@@ -12,16 +12,16 @@ geometry_msgs::PoseStamped Opti_Rect(const geometry_msgs::PoseStamped& input)
     // Maintain the same header
     output.header.seq= input.header.seq;
     // Re-map the x->x, y->z, z->-y (from input to output)...x->x, y->-z, z->y (from output to input)
-    output.transform.translation.x = input.transform.translation.x;
-    output.transform.translation.y = input.transform.translation.y;
-    output.transform.translation.z = input.transform.translation.z;
+    output.pose.position.x = input.pose.position.x;
+    output.pose.position.y = -input.pose.position.z;
+    output.pose.position.z = input.pose.position.y;
     
     // This is necessary because the OptiTrack appears to internally use a left-handed coordinate system.
     // The switching and inversion of the y and z components of the output pose appear to fix this.
-    output.transform.rotation.x = input.transform.rotation.x;
-    output.transform.rotation.y = input.transform.rotation.y;
-    output.transform.rotation.z = input.transform.rotation.z;
-    output.transform.rotation.w = input.transform.rotation.w;
+    output.pose.orientation.x = input.pose.orientation.x;
+    output.pose.orientation.y = -input.pose.orientation.z;
+    output.pose.orientation.z = input.pose.orientation.y;
+    output.pose.orientation.w = input.pose.orientation.w;
 
     return output;
 }//End of function Opti_Rect
